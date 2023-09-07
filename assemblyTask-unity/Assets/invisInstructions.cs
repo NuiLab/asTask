35702c01c59729611311c28c74eb284de9442cb4
Lucas <lplabst@colostate.edu>
2023-09-07 16:07:19 -0600

    added textpanel and continue button

--- assemblyTask-unity/Assets/invisInstructions.cs
+++ assemblyTask-unity/Assets/invisInstructions.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class invisInstructions : MonoBehaviour
 {
     public GameObject[] instructionBars;
+    public string[] instructionTexts;
     public Material builtMat;
     public int currentStep = 0;
     public int mistakes;
     public bool stepByStep;
+    public TMP_Text instructionPanel;
+    public GameObject builtShape;
     // Start is called before the first frame update
     void Start()
     {
-        if(!stepByStep){
+        instructionPanel.text = instructionTexts[currentStep];
+        if (!stepByStep)
+        {
             foreach (GameObject bar in instructionBars)
             {
                 bar.SetActive(true);
@@ -34,8 +40,14 @@ public class invisInstructions : MonoBehaviour
             {
                 currentStep++;
                 instructionBars[currentStep].SetActive(true);
+                instructionPanel.text = instructionTexts[currentStep];
                 // need to add a check for the last step
             }
         }
+    }
+    public void showBuiltShape()
+    {
+        builtShape.SetActive(true);
+        // muss hier noch alles andere ausschalten um sicherzustellen dass quasi pausiert ist
     }
 }
