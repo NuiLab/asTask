c585214be9113b6c2b066e91987f5f88964718c2
Lucas <lplabst@colostate.edu>
2023-11-14 16:00:22 -0700

    added all new shapes, need to adapt scenes

---
+++ assemblyTask-unity/Assets/ShapePreview.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapePreview : MonoBehaviour
+{
+    public Material newMaterial; // Define your material here
+    public bool isGray = true;
+    public bool considerColor = false;
+    public bool isPreview = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!considerColor&&isPreview)
+        {
+            SetPropCheckColorToNull(this.transform);
+            foreach (Transform child in transform)
+            {
+                MeshRenderer meshRenderer = child.GetComponent<MeshRenderer>();
+                if (meshRenderer != null)
+                {
+                    meshRenderer.material = newMaterial;
+                }
+            }
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SetPropCheckColorToNull(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            propCheck propCheck1 = child.GetComponent<propCheck>();
+            if (propCheck1 != null)
+            {
+                propCheck1.color = null;
+            }
+
+            // Recursively set color to null for children's children
+            SetPropCheckColorToNull(child);
+        }
+    }
+}
--- assemblyTask-unity/Assets/WidgetPreview.cs
+++ assemblyTask-unity/Assets/WidgetPreview.cs
@@ -1,15 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
-public class propCheck : MonoBehaviour
+public class WidgetPreview : MonoBehaviour
 {
-    // this is just to check if the bar has the same length as the instruction one
-    public int barlength;
-    public string color;
-
-    public 
     // Start is called before the first frame update
     void Start()
     {
