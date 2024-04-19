3d4ba3ea4ac312c82577d737fa2d281102810092
Lucas <lplabst@colostate.edu>
2024-04-19 15:58:32 -0600

    1b seems to work correctly, need to fix all the shjapes besides A, button broken

--- assemblyTask-unity/Assets/invisiBuild.cs
+++ assemblyTask-unity/Assets/invisiBuild.cs
@@ -89,7 +89,15 @@ public class invisiBuild : MonoBehaviour
         {
             correctPlacement = false;
             //Debug.Log("Exited" + other.name);
+        }
+        if (other.CompareTag("WorkbenchTest"))
+        {
+            IsCloseToWorkbench = false;
+        }
+        if (other.CompareTag("alignment"))
+        {
             isAligned = false;
+            //Debug.Log("Aligned");
         }
     }
     void OnTriggerEnter(Collider other)
@@ -100,9 +108,9 @@ public class invisiBuild : MonoBehaviour
         }
         if (other.CompareTag("instruction"))
         {
-           
+
             //Debug.Log("Entered" + other.name);
-            
+
         }
 
     }
@@ -150,7 +158,7 @@ public class invisiBuild : MonoBehaviour
                 // button presses
                 InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Left, leftHandDevices);
                 InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right, rightHandDevices);
-
+                Debug.Log("Is correct placement:" + correctPlacement + "workbench " + IsCloseToWorkbench + " Aligned " + isAligned);
                 bool rightTrigger = false;
                 bool leftTrigger = false;
                 if (rightHandDevices[0] != null)
@@ -169,8 +177,6 @@ public class invisiBuild : MonoBehaviour
                             }
                             if (rightTrigger && !correctPlacement && IsCloseToWorkbench)
                             {
-                                //Debug.Log(correctPlacement + "cp " + "workbench " + IsCloseToWorkbench);
-                                //Debug.Log("aligned " + isAligned);
                                 canBeBuilt = false;
                                 StartCoroutine("WrongBar");
                                 //Debug.Log(errortype);
@@ -230,6 +236,7 @@ public class invisiBuild : MonoBehaviour
 
     IEnumerator build()
     {
+        
         this.gameObject.GetComponent<XROffsetGrabInteractable>().interactionLayerMask = 0;
         // when a bar is placed, it goes back to the original position and a new bar is created instead that cannot be picked up again.
         GameObject newBar = Instantiate(this.gameObject, lastTouchedBar.transform.position, lastTouchedBar.transform.rotation);
@@ -241,9 +248,10 @@ public class invisiBuild : MonoBehaviour
 
         if (RepeatCheck())//if step is repeated, bar fades out and they have to go again.
         {
+            Debug.Log("Repeat in build");
             instructions.GetComponent<invisInstructions>().ArrowRepeat();
             yield return new WaitForSeconds(0.25f);
-            FadeOut(newBar.gameObject, 2f);
+            FadeOutPlaced(newBar);
             this.transform.position = startPos;
             this.transform.rotation = originalRotation;
             yield return new WaitForSeconds(.5f);
@@ -253,17 +261,45 @@ public class invisiBuild : MonoBehaviour
         }
         else
         {
+            Debug.Log("No repeat in build");
+            inst.ArrowNext();
             yield return new WaitForSeconds(0.25f);
             this.transform.position = startPos;
             this.transform.rotation = originalRotation;
             this.gameObject.GetComponent<XROffsetGrabInteractable>().interactionLayerMask = 1;
             manager.GetComponent<ExperimentLog>().AddData(this.gameObject.name, "Correct placement", inst.currentStep.ToString());
-            instructions.GetComponent<invisInstructions>().nextStep();
-            instructions.GetComponent<invisInstructions>().builtBars.Append(newBar.gameObject);
+            inst.nextStep();
+            inst.builtBars.Append(newBar.gameObject);
 
         }
 
         StartCoroutine("resetCanBeBuilt");
+    }
+    public void FadeOutPlaced(GameObject bar)
+    {
+        StartCoroutine(FadeOutPlacedR(bar, 1f));
+
+    }
+    private IEnumerator FadeOutPlacedR(GameObject obj, float duration)
+    {
+        Renderer renderer = obj.GetComponent<Renderer>();
+        Material originalMaterial = renderer.material;
+
+        // Create a new material from fadeMaterial and set its color to the original material's color
+        Material newMaterial = Instantiate(inst.fadingMaterial);
+        newMaterial.color = originalMaterial.color;
+        renderer.material = newMaterial;
+
+        // Fade out the new material
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            float alpha = Mathf.Lerp(1, 0, t / duration);
+            newMaterial.color = new Color(newMaterial.color.r, newMaterial.color.g, newMaterial.color.b, alpha);
+
+            yield return null;
+        }
+
+        obj.SetActive(false);
     }
     IEnumerator buildTransfer()
     {
@@ -291,7 +327,7 @@ public class invisiBuild : MonoBehaviour
         newBar.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         newBar.gameObject.GetComponent<XROffsetGrabInteractable>().enabled = false;
         newBar.gameObject.GetComponent<invisiBuild>().enabled = false;
-        instructions.GetComponent<invisInstructions>().nextStep();
+        inst.nextStep();
         this.transform.position = startPos;
         this.transform.rotation = originalRotation;
         yield return new WaitForSeconds(0.3f);
@@ -301,13 +337,19 @@ public class invisiBuild : MonoBehaviour
 
     IEnumerator WrongBar()
     {
+        Debug.Log("Wrong bar");
         if (sceneDirector.experimentType == SceneDirector.ExperimentType.ExpB)
         {
             //instructions.GetComponent<invisInstructions>().dataLog("Bar", "Correct");
             this.gameObject.GetComponent<XROffsetGrabInteractable>().interactionLayerMask = 0;
+            inst.toggleHands(false);
+            if (!crossSpawned)
+            {
+                tempCross = Instantiate(cross, this.transform.position, Quaternion.identity);
+                crossSpawned = true;
+            }
             this.transform.position = startPos;
             this.transform.rotation = originalRotation;
-            inst.toggleHands(false);
             inst.mistakes++;
             inst.SetTempText();
             inst.builtShape.SetActive(true);
@@ -315,11 +357,6 @@ public class invisiBuild : MonoBehaviour
             inst.stepPanel.SetActive(false);
             //instructions.GetComponent<invisInstructions>().dataLog(this.gameObject.name, "incorrect placement", instructions.GetComponent<invisInstructions>().currentStep.ToString());
             manager.GetComponent<ExperimentLog>().AddData(this.gameObject.name, "Error", inst.currentStep.ToString(), errortype);
-            if (!crossSpawned)
-            {
-                tempCross = Instantiate(cross, this.transform.position, Quaternion.identity);
-                crossSpawned = true;
-            }
             inst.FadeInCorrectBar();
             yield return new WaitForSeconds(2f);
             //shouldNotify = true;
@@ -327,8 +364,6 @@ public class invisiBuild : MonoBehaviour
             inst.cross = tempCross;
             crossSpawned = false;
             StartCoroutine("resetCanBeBuilt");
-
-
         }
         else // experiment A here
         {
@@ -391,29 +426,7 @@ public class invisiBuild : MonoBehaviour
         errortype = "placement";
 
     }
-    public void FadeOut(GameObject obj, float duration)
-    {
-        StartCoroutine(FadeOutRoutine(obj, duration));
-    }
 
-    private IEnumerator FadeOutRoutine(GameObject obj, float duration)
-    {
-        obj.GetComponent<XROffsetGrabInteractable>().interactionLayerMask = 0;
-        //Renderer renderer = obj.GetComponent<Renderer>();
-        //Material material = renderer.material;
-        //Color initialColor = material.color;
-
-        for (float t = 0; t < duration; t += Time.deltaTime)
-        {
-            // float alpha = Mathf.Lerp(initialColor.a, 0, t / duration);
-            //material.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
-            yield return null;
-        }
-
-        // material.color = new Color(initialColor.r, initialColor.g, initialColor.b, 0);
-        //yield return new WaitForSeconds(0.2f);
-        Destroy(obj);
-    }
 }
 
 
